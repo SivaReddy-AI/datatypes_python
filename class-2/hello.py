51d9b01145b204345a0619d0968248f0c6b974ad
Siva Reddy <sivadeveloper02@gmail.com>
2024-11-25 22:31:58 +0000

    new

---
+++ class-2/hello.py
@@ -0,0 +1,11 @@
+list = [12,2,3,4,5,6,7,8,8]
+tuple = (1,2,4,5,6)
+sets = {"name", "email", "phone"}
+dict = {
+    "name": "siva",
+     "email": "abc@gmail.com",
+     "phone":1233444,
+     "course":"Python_"
+
+}
+print(dict)
